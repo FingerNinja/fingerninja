54c5ded5150f0eedc781078faa410d286729ec6a
unknown <pmtcardoso@gmail.com>
2014-06-01 00:35:00 +0100

    612014

---
+++ finger_ninja/Assets/MovePlayer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+	public float distance  = 4.5f;
+	public GameObject explosionPrefab;
+
+	bool tapped = false;
+	bool down = false;
+	// Use this for initialization
+	void Start () {
+		tapped = false;
+		down = false;
+	}
+
+	void Update()
+	{
+
+		if (Input.GetMouseButton (0)) {
+			tapped = true;
+		}
+		if (Input.GetMouseButtonDown (0)) {
+			down = true;
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(down){
+			Vector3 pos = transform.position;
+			pos.z = 1;
+			GameObject explosion = (GameObject)Instantiate(explosionPrefab, pos, transform.rotation);
+			Destroy (explosion, 2);
+			down = false;
+		}
+
+		if(tapped)
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    
+			Vector3 point = ray.origin + (ray.direction * distance);    
+
+			Vector3 pos = transform.position;
+			pos.x = point.x;
+			transform.position = pos;
+			tapped = false;
+		}
+
+	}
+}

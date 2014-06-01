12e140c9eee45c4cd572b0c9a7e4be2944f4abb3
unknown <pmtcardoso@gmail.com>
2014-06-01 21:22:49 +0100

    612014

--- finger_ninja/Assets/MovePlayer.cs
+++ finger_ninja/Assets/MovePlayer.cs
@@ -6,9 +6,13 @@ public class MovePlayer : MonoBehaviour {
 	public float distance  = 1.0f;
 	public GameObject explosionPrefab;
 	public GameObject ghostPrefab;
+	public TrailRenderer trail;
 
 	bool tapped = false;
 	bool down = false;
+
+	public float speed = 10.0f;
+	public float timer = 0f;
 	// Use this for initialization
 	void Start () {
 		tapped = false;
@@ -19,27 +23,53 @@ public class MovePlayer : MonoBehaviour {
 	{
 
 		if (Input.GetMouseButton (0)) {
+
 			tapped = true;
 		}
 		if (Input.GetMouseButtonDown (0)) {
+			trail.startWidth = 0.2f;
+			trail.endWidth = 0.2f;
 			down = true;
+		}
+
+
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    
+		Vector3 point = ray.origin + (ray.direction * distance);    
+		Vector3 v = rigidbody2D.velocity;
+
+		if (down && ((v.x > 0 && transform.position.x >= point.x) || (v.x < 0 && transform.position.x <= point.x))) {
+			Vector3 pos = transform.position;
+			pos.x = point.x;
+			transform.position = pos;
+			v.x = 0;
+			rigidbody2D.velocity = v;
+			down = false;
 		}
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		if(down){
+
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    
+		Vector3 point = ray.origin + (ray.direction * distance);    
+
+
+		if (down) {
+			trail.startWidth = 0f;
+			trail.endWidth = 0f;
 			Vector3 pos = transform.position;
 			pos.z = 1;
-			GameObject explosion = (GameObject)Instantiate(ghostPrefab, pos, transform.rotation);
+			GameObject explosion = (GameObject)Instantiate (ghostPrefab, pos, transform.rotation);
 			Destroy (explosion, 2);
 			down = false;
-		}
-
-		if(tapped)
+			timer = 0.3f;
+		} else if(tapped)
 		{
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    
-			Vector3 point = ray.origin + (ray.direction * distance);    
+			if(timer <= 0f)
+			{	trail.startWidth = 0.2f;
+				trail.endWidth = 0.2f;
+			}else
+				timer -= Time.deltaTime;
 
 			Vector3 pos = transform.position;
 			pos.x = point.x;
@@ -47,5 +77,6 @@ public class MovePlayer : MonoBehaviour {
 			tapped = false;
 		}
 
+		timer -= Time.deltaTime;
 	}
 }

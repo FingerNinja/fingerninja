000dcd5c844532f726e1fdab3374b243329b7f54
unknown <pmtcardoso@gmail.com>
2014-06-02 22:48:13 +0100

    622014

---
+++ finger_ninja/Assets/GameLogic.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+	// Use this for initialization
+	int level;
+	float timer = 0.0f;
+	public GameObject shurikenPrefab;
+
+	void Start () {
+		level = 1;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		timer -= Time.deltaTime;
+		if (timer <= 0) {
+			for(int i = 0; i < level; i++)
+			{
+				Instantiate(shurikenPrefab, new Vector3(Random.Range(-2.5f, 2.5f), Random.Range(7, 15), 0), Quaternion.identity);
+
+			}
+			level++;
+
+			timer = 1.0f;
+
+		}
+	}
+}
---
+++ finger_ninja/Assets/MoveShuriken.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveShuriken : MonoBehaviour {
+
+	public static int ZIGZAG = 2;
+	public static int STRAIGHT_DOWN = 1;
+
+	// Use this for initialization
+	float speed = 7.0f;
+	public int moveType;
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 v = rigidbody2D.velocity;
+		v.y = -speed;
+		rigidbody2D.velocity = v;
+	}
+}
